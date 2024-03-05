adeebf2b35b3c6f4910ca1494975f099756ce643
Kevin <kevin.paghadal@zealousweb.com>
2024-03-05 14:20:37 +0530

    "second commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import MultipleSelect from "./components/MultipleSelect";
+import Book from "./Book";
 
 function App() {
+  const [inputValues, setInputValues] = useState({});
+  const [counter, setCounter] = useState(0);
+  const [selectedbook, setBoook] = useState();
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
+  const handleOnChange = (e) => {
+    const abc = {};
+    abc[e.target.className] = e.target.value;
+    setInputValues({ ...inputValues, ...abc });
+  };
+
+  // console.log(Book[0].name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {console.log(selectedbook)}
+      <div className="App">Study Management</div>
+      <h1> Create New Plan</h1>
+      <div>
+        <h3> Title of Your Plan</h3>
+        <input name="title" />
+      </div>
+
+      <div>
+        {/* <hr /> */}
+        <h3> Select Books</h3>
+
+        {Object.keys(inputValues).map((c) => {
+          return (
+            <>
+              <p>{inputValues[c]}</p>
+            </>
+          );
+        })}
+
+        {Array.from(Array(counter)).map((c, index) => {
+          return (
+            <>
+              name:{" "}
+              <select
+                name="selectedbook"
+                onChange={(e) => {
+                  setBoook(e.target.value);
+                }}
+              >
+                {Object.keys(Book).map((item) => (
+                  <option key={item} value={Book[item]?.name}>
+                    {Book[item]?.name}
+                  </option>
+                ))}
+              </select>
+              Chapter: <MultipleSelect selectedbook={selectedbook} />
+              <br />
+            </>
+          );
+        })}
+        <button onClick={handleClick}>+</button>
+      </div>
+    </>
   );
 }
 
